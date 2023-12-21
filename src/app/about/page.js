5f5dc076a56b24b68d43484c153d751cd84b01c5
devid-koch <devidkoch33@gmail.com>
2023-12-21 16:46:13 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto space-y-4 p-4">
+        <h1 className="text-2xl font-medium text-amber-600">ABOUT US</h1>
+        <p>
+           Hi there, I'm Devid Koch, a full-stack developer 
+           hailing from the serene landscapes of Assam.
+        </p>
+        <p>
+          
+        </p>
+        <p>
+          
+        </p>
+        <p>
+          
+        </p>
+        
+      </div>
+    </>
+  );
+};
+
+export default About;
